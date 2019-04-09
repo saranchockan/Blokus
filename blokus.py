2eae617c080657a98d9627c216f1b1a918ebc5b6
Saran Chockan <saranjitharunesh@gmail.com>
2019-04-08 20:45:43 -0500

    constructed basic grid

---
+++ blokus.py
@@ -0,0 +1,58 @@
+import pygame
+import random
+
+pygame.font.init()
+
+# GLOBALS VARS
+s_width = 800
+s_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600  # meaning 600 // 20 = 20 height per block
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2  # top left x position of play area
+top_left_y = s_height - play_height  # top left y position of play area
+
+
+
+def main_menu():
+
+    grid = create_grid()
+    win = pygame.display.set_mode((s_width, s_height))
+    pygame.display.set_caption('Tetris')
+
+    draw_grid(win,grid)
+
+
+
+def create_grid(locked_positions = {}):
+    grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (j, i) in locked_positions:
+                c = locked_positions[(j, i)]
+                grid[i][j] = c
+    return grid
+
+
+def draw_grid(surface, grid):
+
+    surface.fill((0, 0, 0))
+
+    
+    sx = top_left_x
+    sy = top_left_y
+
+    for i in range(len(grid)):
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i*block_size),
+                         (sx+play_width, sy + i*block_size))  # draws horizontal lines
+        for j in range(len(grid[i]) + 1):
+            pygame.draw.line(surface, (128, 128, 128), (sx + j *
+                                                        block_size, sy), (sx+j*block_size, sy + i*play_height))
+    
+    pygame.display.update()
+
+
+while True:
+    main_menu()
+
