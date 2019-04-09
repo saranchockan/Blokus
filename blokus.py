21ebeb5004317a070d45e2b84f8a9960bf2d10ca
Saran Chockan <saranjitharunesh@gmail.com>
2019-04-09 10:00:25 -0500

    fixed grid

--- blokus.py
+++ blokus.py
@@ -6,12 +6,13 @@ pygame.font.init()
 # GLOBALS VARS
 s_width = 800
 s_height = 700
-play_width = 600  # meaning 600 // 10 = 30 width per block
+play_width = 300  # meaning 300 // 10 = 30 width per block
 play_height = 600  # meaning 600 // 20 = 20 height per block
 block_size = 30
 
 top_left_x = (s_width - play_width) // 2  # top left x position of play area
 top_left_y = s_height - play_height  # top left y position of play area
+
 
 
 
@@ -26,7 +27,7 @@ def main_menu():
 
 
 def create_grid(locked_positions = {}):
-    grid = [[(0, 0, 0) for _ in range(20)] for _ in range(20)]
+    grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (j, i) in locked_positions:
@@ -40,16 +41,19 @@ def draw_grid(surface, grid):
     surface.fill((0, 0, 0))
 
     
-    sx = top_left_x
-    sy = top_left_y
+    sx = 2
+    sy = 2
 
     for i in range(len(grid)):
         pygame.draw.line(surface, (128, 128, 128), (sx, sy + i*block_size),
                          (sx+play_width, sy + i*block_size))  # draws horizontal lines
         for j in range(len(grid[i]) + 1):
             pygame.draw.line(surface, (128, 128, 128), (sx + j *
-                                                        block_size, sy), (sx+j*block_size, sy + i*play_height))
-    
+                                                        block_size, sy), (sx+j*block_size, sy + play_height))
+
+    pygame.draw.line(surface, (128, 128, 128), (sx, sy + len(grid)*block_size),
+                         (sx+play_width, sy + len(grid)*block_size)) 
+
     pygame.display.update()
 
 
